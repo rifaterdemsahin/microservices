780e3d87df5a8184050c3eac2d362ac667cb6daf
Erdem <rifaterdemsahin@gmail.com>
2025-02-17 22:59:40 +0000

    properties and test added

---
+++ 6_Symbols/3_middleware/src/test/java/com/example/MainClassTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainClassTest {
+
+    @Test
+    void contextLoads() {
+        // Test if the application context loads correctly
+    }
+}
