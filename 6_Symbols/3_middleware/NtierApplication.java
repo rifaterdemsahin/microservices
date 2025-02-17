fa0b0aa0a972d5abb1eb1a9bfb02421a2c9c62e9
Erdem <rifaterdemsahin@gmail.com>
2025-02-17 21:21:39 +0000

    main class fixed

--- 6_Symbols/3_middleware/NtierApplication.java
+++ 6_Symbols/3_middleware/NtierApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NtierApplication {
+public class MainClass {
     public static void main(String[] args) {
-        SpringApplication.run(NtierApplication.class, args);
+        SpringApplication.run(MainClass.class, args);
     }
 }
